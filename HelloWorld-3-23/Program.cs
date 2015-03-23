56c29b4bad19b42000cf456f5b091f8f0c189019
Seth Johnson <sethjdesigns@gmail.com>
2015-03-23 13:45:14 -0400

    Print FirstName LastName
    Prints FirstName followed by LastName

--- HelloWorld-3-23/Program.cs
+++ HelloWorld-3-23/Program.cs
@@ -10,8 +10,14 @@ namespace HelloWorld_3_23
     {
         static void Main(string[] args)
         {
-            //Print the string "Hello" and concatinate the Main argument.
-            Console.WriteLine("Hello "+ args[0]);
+            //Print the string "Hello" and concatinate the Main argument at index '0'
+            //Console.WriteLine("Hello "+ args[0]);
+
+
+            //Print FirstName LastName
+            //Print args string index 0 and 1
+            Console.Write(args[0] + " " + args[1]);
+
             //Read Line
             Console.ReadLine();
         }
