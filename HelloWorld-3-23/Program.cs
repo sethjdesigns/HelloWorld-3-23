86e943d7697134094df3db19bca84830a7971888
Seth Johnson <sethjdesigns@gmail.com>
2015-03-23 13:35:35 -0400

    First Commit
    Takes args array and prints the 0 index which is equal to the string
    "Seth"

--- HelloWorld-3-23/Program.cs
+++ HelloWorld-3-23/Program.cs
@@ -10,6 +10,10 @@ namespace HelloWorld_3_23
     {
         static void Main(string[] args)
         {
+            //Print the string "Hello" and concatinate the Main argument.
+            Console.WriteLine("Hello "+ args[0]);
+            //Read Line
+            Console.ReadLine();
         }
     }
 }
